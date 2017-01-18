a6daf0854ca8e17a883ecb82bfa25a5a08817d3b
Jitendra Kumar <jitendra.kumar@tothenew.com>
2017-01-18 21:25:12 +0530

    Update package and add gitignore

---
+++ src/main/java/onsterlabs/network/rxnetwork/APISubscriber.java
@@ -0,0 +1,38 @@
+package onsterlabs.network.rxnetwork;
+
+import java.io.IOException;
+
+import retrofit2.Response;
+import retrofit2.adapter.rxjava.HttpException;
+import rx.Subscriber;
+
+public class APISubscriber<T> extends Subscriber<T> {
+
+    @Override
+    public void onCompleted() {
+        // TODO To be evaluated
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+        RXEventBus.getInstance().post(getRetroError(throwable));
+    }
+
+    @Override
+    public void onNext(T t) {
+        RXEventBus.getInstance().post(t);
+    }
+
+    public RetroError getRetroError(Throwable throwable){
+
+        if (throwable instanceof HttpException) {
+            HttpException httpException = (HttpException) throwable;
+            Response response = httpException.response();
+            return new RetroError(RetroError.Kind.HTTP, response.message(),response.code());
+        }
+
+        return throwable instanceof IOException?new RetroError(RetroError.Kind.NETWORK, throwable.getMessage(),-999): new RetroError(RetroError.Kind.UNEXPECTED, throwable.getMessage(), -999);
+
+    }
+
+}
---
+++ src/main/java/onsterlabs/network/rxnetwork/RxErrorHandlingCallAdapterFactory.java
@@ -0,0 +1,73 @@
+package onsterlabs.network.rxnetwork;
+
+import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+
+import onsterlabs.network.RetrofitException;
+import retrofit2.Call;
+import retrofit2.CallAdapter;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.HttpException;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import rx.Observable;
+
+/**
+ * Created by ttnd on 20/10/16.
+ */
+public class RxErrorHandlingCallAdapterFactory extends CallAdapter.Factory {
+
+    private final RxJavaCallAdapterFactory original;
+
+    private RxErrorHandlingCallAdapterFactory() {
+        original = RxJavaCallAdapterFactory.create();
+    }
+
+    public static CallAdapter.Factory create() {
+        return new RxErrorHandlingCallAdapterFactory();
+    }
+
+    @Override
+    public CallAdapter<?> get(Type returnType, Annotation[] annotations, Retrofit retrofit) {
+        return new RxCallAdapterWrapper(retrofit, original.get(returnType, annotations, retrofit));
+    }
+
+    private static class RxCallAdapterWrapper implements CallAdapter<Observable<?>> {
+        private final Retrofit retrofit;
+        private final CallAdapter<?> wrapped;
+
+        public RxCallAdapterWrapper(Retrofit retrofit, CallAdapter<?> wrapped) {
+            this.retrofit = retrofit;
+            this.wrapped = wrapped;
+        }
+
+        @Override
+        public Type responseType() {
+            return wrapped.responseType();
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public <R> Observable<?> adapt(Call<R> call) {
+            return ((Observable) wrapped.adapt(call));
+        }
+
+        private RetrofitException asRetrofitException(Throwable throwable) {
+            // We had non-200 http error
+            if (throwable instanceof HttpException) {
+                HttpException httpException = (HttpException) throwable;
+                Response response = httpException.response();
+                return RetrofitException.httpError(response.raw().request().url().toString(), response, retrofit);
+            }
+            // A network error happened
+            if (throwable instanceof IOException) {
+                return RetrofitException.networkError((IOException) throwable);
+            }
+
+            // We don't know what happened. We need to simply convert to an unknown error
+
+            return RetrofitException.unexpectedError(throwable);
+        }
+    }
+}
